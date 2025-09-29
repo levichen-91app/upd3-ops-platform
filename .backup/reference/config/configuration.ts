6b7c703a58ec91ede7e6eb529c6b438bdb10944d
Levi Chen <levichen@MacBookPro.localdomain>
2025-09-29 10:51:29 +0800

    remove back files

--- .backup/reference/config/configuration.ts
+++
@@ -1,7 +0,0 @@
-import appConfig from './app.config';
-import externalApisConfig from './external-apis.config';
-
-/**
- * Main configuration export - combines all configuration modules
- */
-export default [appConfig, externalApisConfig];
